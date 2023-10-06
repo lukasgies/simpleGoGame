97f28eabd1a48ec1b1de5fb0b02a5dbc4ff93fb8
Lukas Gies <lukas.gies@moguru.de>
2023-10-06 09:23:42 +0200

    Initial Commit

---
+++ src/main/java/de/lukasgies/simpleGoGame/simpleGoGameServer/SimpleGoGameServerApplication.java
@@ -0,0 +1,13 @@
+package de.lukasgies.simpleGoGame.simpleGoGameServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGoGameServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleGoGameServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/lukasgies/simpleGoGame/simpleGoGameServer/SimpleGoGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lukasgies.simpleGoGame.simpleGoGameServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGoGameServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
